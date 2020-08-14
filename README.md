00554a558b22663e78a104a1d1c10f312a88ad5b
kadu <kadu@kadu.com.br>
2020-08-13 21:56:31 -0300

    Some ideas to control buttons

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 This is a cheap version of (elGato) Stream Deck, made using Arduino Pro Mini (or Leonardo)
 
 Tinkercad prototype: https://www.tinkercad.com/things/kq3zCQsBvtN
+
+
+** Work in progress **
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
 #include <Arduino.h>
 #include "Keyboard.h"
 
-bool showMessage = false;
-String message;
+void pressedButton(int value) {
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press(KEY_LEFT_SHIFT);
+
+  if(value < 200) {
+    Keyboard.press('X');
+  } else if(value < 390) {
+    Keyboard.press('Y');
+  } else if(value < 900) {
+    Keyboard.press('Z');
+  }
+  delay(50);
+  Keyboard.releaseAll();
+  delay(300);
+}
 
 void setup() {
   Serial.begin(115200);
@@ -11,32 +25,11 @@ void setup() {
   Serial.println("Stream Deck Ready!");
 }
 
-
 void loop() {
   int value = analogRead(A0);
   delay(50);
 
-  if(value > 10) {
-    if( value < 200) {
-      message = "Button 1";
-      showMessage = true;
-    } else if(value < 390) {
-      message = "Button 2";
-      showMessage = true;
-    } else if(value < 900) {
-      message = "Button 3";
-      showMessage = true;
-    }
-  }
-
-  if(showMessage) {
-    Keyboard.press(KEY_LEFT_CTRL);
-    Keyboard.press(KEY_LEFT_ALT);
-    Keyboard.press(KEY_LEFT_SHIFT);
-    Keyboard.press('X');
-    delay(100);
-    Keyboard.releaseAll();
-    delay(1000);
-    showMessage = false;
+  if(value > 10) { // if some button was pressed
+    pressedButton(value);
   }
 }
